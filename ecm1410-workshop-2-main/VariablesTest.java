d57ad4dda9c9a4d56a2746beb6e5f283efb87f46
os492 <os492@exeter.ac.uk>
2026-02-01 16:23:34 +0000

    variables test different data types

--- ecm1410-workshop-2-main/VariablesTest.java
+++ ecm1410-workshop-2-main/VariablesTest.java
@@ -3,10 +3,17 @@ public class VariablesTest {
     public static void main(String[] args) {
         int x = 4;
         System.out.println("x: " + x);
-        byte s_per_min = 60;
-        long s_per_day = (s_per_min * 60 * 24);
+        long s_per_min = 60;
+        byte s_per_day = (byte) (s_per_min * 60 * 24);
+        float area = 20.3F;
+        char a = '\u5468', b = '\\';
+        int nHours = 022;
+        double ratio = 7/5;
         System.out.println("Seconds per minute: " + s_per_min );
         System.out.println("Seconds per day: " + s_per_day);
-    
+        System.out.println("Area: " + area);
+        System.out.println("A: " + a);
+        System.out.println("Number of Hours: " + nHours);
+        System.out.println("Ratio: " + ratio); 
     }
 }
