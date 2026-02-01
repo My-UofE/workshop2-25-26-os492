880dd56234f1a3a7f92a8a65b1c2026a67f99d81
os492 <os492@exeter.ac.uk>
2026-02-01 16:26:39 +0000

    variables test epsilon

--- ecm1410-workshop-2-main/VariablesTest.java
+++ ecm1410-workshop-2-main/VariablesTest.java
@@ -15,5 +15,17 @@ public class VariablesTest {
         System.out.println("A: " + a);
         System.out.println("Number of Hours: " + nHours);
         System.out.println("Ratio: " + ratio); 
+
+        double d1 = 0.29/0.0001;
+        double d2 = 0.00029*1.0e7;
+        System.out.println("d1: " + d1);
+        System.out.println("d2: " + d2);
+        System.out.println("d1 equals d2: " + (d1 == d2) );
+
+        double epsilon = 1e-10;
+        if (Math.abs(d1 - d2) < epsilon) {
+            System.out.print("d1,d2 differ by less than ");
+            System.out.println( epsilon );
+        }    
     }
 }
