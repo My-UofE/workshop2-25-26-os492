431a02b6804455ec69d870390252605c1cc54bbf
os492 <os492@exeter.ac.uk>
2026-02-01 16:20:00 +0000

    variables test seconds

---
+++ ecm1410-workshop-2-main/VariablesTest.java
@@ -0,0 +1,12 @@
+// VariablesTest application
+public class VariablesTest {
+    public static void main(String[] args) {
+        int x = 4;
+        System.out.println("x: " + x);
+        byte s_per_min = 60;
+        long s_per_day = (s_per_min * 60 * 24);
+        System.out.println("Seconds per minute: " + s_per_min );
+        System.out.println("Seconds per day: " + s_per_day);
+    
+    }
+}
