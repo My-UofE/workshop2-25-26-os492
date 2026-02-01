040b8a87579c70aaa01103a694b2b03232b32a13
os492 <os492@exeter.ac.uk>
2026-02-01 16:08:24 +0000

    hello world name

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,3 +1,8 @@
 // Add your code below
-
+// HelloWorld application
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World! My name is" + args[0]);
+    }
+}
 
