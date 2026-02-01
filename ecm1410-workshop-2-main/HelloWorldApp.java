2e7b84e842686f2563ed19cef753b804705bca85
os492 <os492@exeter.ac.uk>
2026-02-01 16:12:55 +0000

    hello world age and name

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -2,7 +2,7 @@
 // HelloWorld application
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World! My name is " + args[0]);
+        System.out.println("Hello World! My name is " + args[0] + ". I am " + args[1] + " years old.");
     }
 }
 
